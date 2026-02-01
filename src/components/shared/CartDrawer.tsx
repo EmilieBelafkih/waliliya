57c5a2c1425caf5f2b4cf775db94ac25718eaec7
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2026-02-01 16:48:30 +0300

    checkout button update

--- src/components/shared/CartDrawer.tsx
+++ src/components/shared/CartDrawer.tsx
@@ -252,7 +252,7 @@ function CheckoutButton() {
       {pending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-2 border-white border-t-transparent" />
       ) : (
-        'Continuer'
+        "J'achète"
       )}
     </button>
   );
