162002fe90d893778c81479165e34406726ed246
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2026-02-01 14:50:48 +0300

    cart context update

--- src/app/ClientProviders.tsx
+++ src/app/ClientProviders.tsx
@@ -2,7 +2,6 @@
 
 import { CartProvider } from '@/context/CartContext';
 import { Cart } from '@/lib/shopify/types';
-import { useEffect } from 'react';
 
 export default function ClientProviders({
   children,
@@ -11,12 +10,5 @@ export default function ClientProviders({
   children: React.ReactNode;
   cart: Cart | undefined;
 }) {
-  useEffect(() => {
-    if (!cart) {
-      document.cookie =
-        'cartId=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    }
-  }, [cart]);
-
   return <CartProvider cart={cart}>{children}</CartProvider>;
 }
